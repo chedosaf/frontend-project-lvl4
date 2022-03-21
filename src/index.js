801c1175a6e777c0cea55efcb4ad3dd11172e93d
Sasha Chadaev <chedosaf@mail.ru>
2022-03-22 00:03:35 +0400

    adding project to Rollbar check

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
+import '../assets/application.scss';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import AppFinal from './index.jsx';
+
+if (process.env.NODE_ENV !== 'production') {
+  localStorage.debug = 'chat:*';
+}
+
+ReactDOM.render(
+  React.createElement(AppFinal), document.querySelector('#chat'),
+);
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,9 @@
 // @ts-check
-
-import 'core-js/stable/index.js';
-import 'regenerator-runtime/runtime.js';
-import '../assets/application.scss';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import App from './components/app.jsx';
-
-if (process.env.NODE_ENV !== 'production') {
-  localStorage.debug = 'chat:*';
-}
 
 const AppFinal = () => (
   <I18nextProvider i18n={i18n}>
@@ -23,4 +15,4 @@ ReactDOM.render(
   <AppFinal />, document.querySelector('#chat'),
 );
 
-console.log('it works now!');
+export default AppFinal;
