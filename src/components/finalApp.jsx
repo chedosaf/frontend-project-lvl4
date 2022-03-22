25e3c2672a2188e28dc5ffe9eab9a01b2815f745
Sasha Chadaev <chedosaf@mail.ru>
2022-03-22 18:01:41 +0400

    change entry point

--- src/components/finalApp.jsx
+++ src/components/finalApp.jsx
@@ -1,18 +1,13 @@
 // @ts-check
-import ReactDOM from 'react-dom';
 import React from 'react';
 import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
-import App from './components/app.jsx';
+import i18n from '../i18n';
+import App from './app.jsx';
 
 const AppFinal = () => (
   <I18nextProvider i18n={i18n}>
     <App />
   </I18nextProvider>
-);
-
-ReactDOM.render(
-  <AppFinal />, document.querySelector('#chat'),
 );
 
 export default AppFinal;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'regenerator-runtime/runtime.js';
 import '../assets/application.scss';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import AppFinal from './index.jsx';
+import AppFinal from './components/finalApp.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
