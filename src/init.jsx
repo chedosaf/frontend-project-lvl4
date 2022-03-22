041749096fe97607f3cc7cccda98373decea8ea3
Sasha Chadaev <chedosaf@mail.ru>
2022-03-22 22:52:21 +0400

    something test

--- src/init.jsx
+++ src/init.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppFinal from './components/finalApp.jsx';
 
-const init = () => {
-  console.log(document.querySelector('#chat'));
-  ReactDOM.render(
-    <AppFinal />, document.querySelector('#chat'),
-  );
-};
+const init = () => <AppFinal />;
 
 export default init;
