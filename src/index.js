7ea983bd378b42f97558777e69d0516e8d76f100
Sasha Chadaev <chedosaf@mail.ru>
2022-03-22 22:29:09 +0400

    edit init

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import '../assets/application.scss';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import AppFinal from './components/finalApp.jsx';
+import init from './init.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-ReactDOM.render(
-  (React.createElement(AppFinal)), document.querySelector('#chat'),
-);
+init();
