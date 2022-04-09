b44d84dbe3090a75f70230da18af383e280c2f7c
Sasha Chadaev <chedosaf@mail.ru>
2022-04-09 16:07:15 +0400

    edit webpack config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -7,50 +7,45 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 
-module.exports = () => {
-  const env = dotenv.config().parsed;
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
-  return {
+module.exports = {
 
-    mode,
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
-    output: {
-      path: path.join(__dirname, 'dist', 'public'),
-      publicPath: '/assets/',
-    },
-    devServer: {
-      compress: true,
-      port: 8080,
-      host: '0.0.0.0',
-      publicPath: '/assets/',
-      historyApiFallback: true,
-    },
-    plugins: [
-      new MiniCssExtractPlugin(),
-      new webpack.DefinePlugin(envKeys),
+  mode,
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist', 'public'),
+    publicPath: '/assets/',
+  },
+  devServer: {
+    compress: true,
+    port: 8080,
+    host: '0.0.0.0',
+    publicPath: '/assets/',
+    historyApiFallback: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
     ],
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: 'babel-loader',
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            { loader: MiniCssExtractPlugin.loader },
-            { loader: 'css-loader' },
-            { loader: 'postcss-loader' },
-            { loader: 'sass-loader' },
-          ],
-        },
-      ],
-    },
-  };
+  },
 };
